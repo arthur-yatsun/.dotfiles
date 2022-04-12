c61dddee6bf86ddd4056ebfb69bfa335d7b7f0af
arthur-yatsun <a_yatsun-itssv@kw.com>
2022-04-12 13:25:23 +0300

    remove toggleterm

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -13,22 +13,24 @@ require "user.colorscheme"
 require "user.lualine"
 require "user.bufferline"
 require "user.treesitter"
+require "user.alpha"
+require "user.indentline"
+require "user.minimap"
+
+-- editor features
 require "user.autopairs"
 require "user.comment"
-
-
-require "user.telescope"
 require "user.gitsigns"
-require "user.nvim-tree"
-
-require "user.py_env"
 require "user.impatient"
--- require "user.minimap"
 
--- require "user.toggleterm"
+
+-- other
+require "user.nvim-tree"
+require "user.telescope"
+-- connects automatically to the active python venv
+require "user.py_env"
+
+
 -- require "user.project"
-require "user.alpha"
--- require "user.indentline"
 -- require "user.whichkey"
 -- require "user.autocommandse
-
--- nvim/.config/nvim/lua/user/minimap.lua
+++ nvim/.config/nvim/lua/user/minimap.lua
@@ -1,6 +1,4 @@
-vim.cmd [[
-   let g:minimap_width = 10
-   let g:minimap_auto_start = 1
-   let g:minimap_auto_start_win_enter = 1 
-]]
+vim.g.minimap_width = 10
+vim.g.minimap_auto_start = 0
+vim.g.minimap_auto_start_win_enter = 1
 
