7620fb6aabcfa730425bce710f12ee93166f0f50
arthur-yatsun <a_yatsun-itssv@kw.com>
2022-04-19 23:49:17 +0300

    set max line length for flake8 diagnostic'

--- nvim/.config/nvim/lua/user/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/user/lsp/null-ls.lua
@@ -14,6 +14,10 @@ null_ls.setup({
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-        diagnostics.flake8
+        diagnostics.flake8.with({extra_args = { "--max-line-length", "90" } }),
 	},
 })
+
+-- increase timeout to 2 seconds
+vim.lsp.buf.formatting_sync(nil, 2000)
+
