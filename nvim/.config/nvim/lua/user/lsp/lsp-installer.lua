b2ad0df6073f505ac2ddecf928015a0e4091222a
arthur-yatsun <arthuryatsun@arthuryatsun.com>
2022-06-24 22:31:33 +0300

    update

--- nvim/.config/nvim/lua/user/lsp/lsp-installer.lua
+++ nvim/.config/nvim/lua/user/lsp/lsp-installer.lua
@@ -15,6 +15,7 @@ lsp_installer.on_server_ready(function(server)
 	 	local jsonls_opts = require("user.lsp.settings.jsonls")
 	 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
 	 end
+     
 
 	 if server.name == "sumneko_lua" then
 	 	local sumneko_opts = require("user.lsp.settings.sumneko_lua")
