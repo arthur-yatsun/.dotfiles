a326ac0b8e69dd4d226c9c39c42ac076f2c91ffb
arthur-yatsun <arthuryatsun@arthuryatsun.com>
2022-06-29 19:57:02 +0300

    add flag to remove personal dotfiles

--- remove.sh
+++ remove.sh
@@ -3,14 +3,31 @@
 set -e -o pipefail
 
 
-for dir in */; do
+remove_dotfiles () {
+    for dir in */; do
+        # skip archive folder
+        if [ "$dir" = "archive/" ]; then 
+           continue 
+        fi
 
-    # skip archive folder
-    if [ "$dir" = "archive/" ]; then 
-       continue 
-    fi
+        stow -vDt ~ $dir
+        echo
+    done
+}
 
-    stow -vDt ~ $dir
-    echo
-done
 
+remove_personal_dotfiles () {
+    cd ./.personal/
+    remove_dotfiles
+}
+
+
+OPTION=$1
+case $OPTION in
+    -p)
+        remove_personal_dotfiles
+        ;;
+    *)
+        remove_dotfiles
+        ;;
+esac
