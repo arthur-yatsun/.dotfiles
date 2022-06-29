f335bb58db486921ba62d7f9daf881feb56a1e90
arthur-yatsun <arthuryatsun@arthuryatsun.com>
2022-06-29 19:50:48 +0300

    add flag to install personal dotfiles

--- install.sh
+++ install.sh
@@ -3,13 +3,31 @@
 set -e -o pipefail
 
 
-for dir in */; do
+install_dotfiles () {
+    for dir in */; do
+        # skip archive folder
+        if [ "$dir" = "archive/" ]; then 
+           continue 
+        fi
 
-    # skip archive folder
-    if [ "$dir" = "archive/" ]; then 
-       continue 
-    fi
+        stow -vt ~ $dir
+        echo
+    done
+}
 
-    stow -vt ~ $dir
-    echo
-done
+
+install_personal_dotfiles () {
+    cd ./.personal/
+    install_dotfiles
+}
+
+
+OPTION=$1
+case $OPTION in
+    -p)
+        install_personal_dotfiles
+        ;;
+    *)
+        install_dotfiles
+        ;;
+esac
