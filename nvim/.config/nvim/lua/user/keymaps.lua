6ab56f6195d7eaaf025ddbadb6ec32076375d15e
arthur-yatsun <arthuryatsun@arthuryatsun.com>
2022-07-08 22:05:53 +0300

    add new keymaps

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -3,10 +3,12 @@ local opts = { noremap = true, silent = true }
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
+
 -- Remap space as leader key
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
 
 -- modes
 --   normal_mode = "n",
@@ -16,6 +18,7 @@ vim.g.maplocalleader = " "
 --   term_mode = "t",
 --   command_mode = "c",
 
+
 -- normal --
 -- better window navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
@@ -23,17 +26,19 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
+
 -- resize with arrows
 keymap("n", "<C-Up>", ":resize -10<CR>", opts)
 keymap("n", "<C-Down>", ":resize +10<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize -10<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize +10<CR>", opts)
 
+
 -- navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 keymap("n", "<M-1>", "<Cmd>BufferLineGoToBuffer 1<CR>", opts)
-keymap("n", "<M-2>", "<Cmd>BufferLineGoToBuffer 2<CR>", opts)
+keymap("n", "<A-2>", "<Cmd>BufferLineGoToBuffer 2<CR>", opts)
 keymap("n", "<M-3>", "<Cmd>BufferLineGoToBuffer 3<CR>", opts)
 keymap("n", "<M-4>", "<Cmd>BufferLineGoToBuffer 4<CR>", opts)
 keymap("n", "<M-5>", "<Cmd>BufferLineGoToBuffer 5<CR>", opts)
@@ -44,13 +49,16 @@ keymap("n", "<M-9>", "<Cmd>BufferLineGoToBuffer 9<CR>", opts)
 keymap("n", "<C-o>", "<Cmd>Bdelete!<cr>", opts)
 keymap("n", "j;", "<Cmd>e#<cr>", opts)
 
+
 -- move text up and down
 keymap("n", "<leader>j", "<Esc>:m .+1<CR>==", opts)
 keymap("n", "<leader>k", "<Esc>:m .-2<CR>==", opts)
 
+
 -- change indent
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
+
 
 -- real copy and paste
 -- https://stackoverflow.com/questions/11993851/how-to-delete-not-cut-in-vim
@@ -58,9 +66,11 @@ keymap("n", "d", '"_d', opts)
 keymap("v", "d", '"_d', opts)
 keymap("v", "p", '"_dP', opts)
 
+
 -- Move text up and down
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+
 
 -- telescope --
 keymap("n", "<leader>a", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ winblend = 10 }))<cr>", opts)
@@ -73,17 +83,23 @@ keymap("n", "<leader>b", "<cmd>Telescope buffers<cr>", opts)
 keymap("n", "<leader>h", '<cmd>lua require("telescope.builtin").git_bcommits()<cr>', opts)
 keymap("n", "<leader>i", '<cmd>lua require("telescope.builtin").git_status()<cr>', opts)
 
+
 -- nvim tree --
 keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
 
+
 -- reload config
-keymap("n", "<leader>z", "<cmd>lua require('nvim-reload').Reload()<cr>", opts)
+keymap("n", "<leader>z", "<cmd>lua require('nvim-reload').Restart()<cr>", opts)
 
 
 -- gitsigns
 keymap("n", "<leader>,", "<cmd>lua require('gitsigns').preview_hunk()<cr>", opts)
 keymap("n", ",<leader>", "<cmd>lua require('gitsigns').reset_hunk()<cr>", opts)
-keymap("n", "<leader>.", "<cmd>lua require('gitsigns').blame_line()<cr>", opts)
+keymap("n", ".", "<cmd>lua require('gitsigns').blame_line()<cr>", opts)
 
 keymap("n", ",", "<cmd>lua require('gitsigns').next_hunk()<cr>", opts)
+keymap("n", "<", "<cmd>lua require('gitsigns').prev_hunk()<cr>", opts)
 
+
+-- minimap
+keymap("n", "<leader>m", ":MinimapToggle<cr>", opts)
--- nvim/.config/nvim/lua/user/minimap.lua
+++ nvim/.config/nvim/lua/user/minimap.lua
@@ -1,6 +1,6 @@
 vim.g.minimap_width = 10
-vim.g.minimap_auto_start = 1
-vim.g.minimap_auto_start_win_enter = 1
+-- vim.g.minimap_auto_start = 1
+-- vim.g.minimap_auto_start_win_enter = 1
 
 -- vim.g.minimap_highlight_range = 1
 vim.g.minimap_highlight_search = 1
@@ -10,6 +10,7 @@ vim.g.minimap_git_colors = 1
 vim.cmd "hi MinimapCurrentLine ctermfg=None guifg=#ABB2BF guibg=none"
 vim.g.minimap_cursor_color = 'MinimapCurrentLine'
 
+vim.g.minimap_left = 1
 
 
 
