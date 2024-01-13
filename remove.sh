1255ec7fb5a4765b3ce01ddc896f63daa453f33b
arthur-yatsun <arthur54342@gmail.com>
2024-01-13 15:24:33 +0200

    remove submodules

--- remove.sh
+++ remove.sh
@@ -1,30 +1,13 @@
 #!/usr/bin/zsh
 
+for dir in */; do
+    # skip dir names started with "."
+    if [[ "$dir" = .* ]]; then
+       echo "skipping ${dir}"
+       continue
+    fi
 
-remove_dotfiles () {
-    for dir in */; do
-        # skip dir names started with "."
-        if [[ "$dir" = .* ]]; then 
-           echo "skipping ${dir}"
-           continue 
-        fi
+    echo "Removing ${dir}"
+    stow -vDt ~ $dir
+done
 
-        echo "Removing ${dir}"
-        stow -vDt ~ $dir
-    done
-}
-
-remove_personal_dotfiles () {
-    cd ./.personal/
-    remove_dotfiles
-}
-
-OPTION=$1
-case $OPTION in
-    -p)
-        remove_personal_dotfiles
-        ;;
-    *)
-        remove_dotfiles
-        ;;
-esac
