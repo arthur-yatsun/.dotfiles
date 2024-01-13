caab44b2efa323e11c491aad49d676d57fbc8fad
arthur-yatsun <arthur54342@gmail.com>
2024-01-13 15:23:16 +0200

    remove submodules

--- install.sh
+++ install.sh
@@ -1,29 +1,13 @@
 #!/bin/zsh
 
-install_dotfiles () {
-    for dir in */; do
-        # skip dir names started with "."
-        if [[ "$dir" = .* ]]; then 
-           echo "skipping ${dir}"
-           continue 
-        fi
-        
-        echo "Installing ${dir}"
-        stow -vt ~ $dir
-    done
-}
+for dir in */; do
+    # skip dir names started with "."
+    if [[ "$dir" = .* ]]; then
+       echo "skipping ${dir}"
+       continue
+    fi
 
-install_personal_dotfiles () {
-    cd ./.personal/
-    install_dotfiles
-}
+    echo "Installing ${dir}"
+    stow -vt ~ $dir
+done
 
-OPTION=$1
-case $OPTION in
-    -p)
-        install_personal_dotfiles
-        ;;
-    *)
-        install_dotfiles
-        ;;
-esac
